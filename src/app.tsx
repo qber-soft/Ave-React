71c22f0655e61af9d3cda7ae82941b858dcbd7fd
rerender2021 <rerender2021@outlook.com>
2022-10-15 11:32:57 +0800

    add TestComboBoxAdd2

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { AveRenderer } from "./ave-react";
 import { startDevtoolBackend } from "./devtool";
-import { TestComboBoxAdd } from "./examples";
+import { TestComboBoxAdd2 } from "./examples";
 
 if (process.env.NODE_ENV === "dev") {
 	startDevtoolBackend();
 }
 
-const app = <TestComboBoxAdd />;
+const app = <TestComboBoxAdd2 />;
 
 AveRenderer.render(app);
--- src/examples/combo-box/index.tsx
+++ src/examples/combo-box/index.tsx
@@ -2,3 +2,4 @@ export * from "./combo-box-basic";
 export * from "./combo-box-update";
 export * from "./combo-box-update-2";
 export * from "./combo-box-add";
+export * from "./combo-box-add-2";
