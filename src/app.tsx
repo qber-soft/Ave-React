0de8935b58ceaee5adc1d1e351e1174e5a8951d6
rerender2021 <rerender2021@outlook.com>
2022-10-16 19:50:33 +0800

    refine TestCheckBoxStyle

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { AveRenderer } from "./ave-react";
 import { startDevtoolBackend } from "./devtool";
-import { TestCheckBoxColor } from "./examples";
+import { TestCheckBoxStyle } from "./examples";
 
 if (process.env.NODE_ENV === "dev") {
 	startDevtoolBackend();
 }
 
-const app = <TestCheckBoxColor />;
+const app = <TestCheckBoxStyle />;
 
 AveRenderer.render(app);
