60b1a174f811e43214469563b568e47da8ceeb52
rerender2021 <rerender2021@outlook.com>
2022-11-05 15:46:20 +0800

    impl data

--- draft/ave-react/components/rich-list-box.tsx
+++ draft/ave-react/components/rich-list-box.tsx
@@ -1,25 +1,31 @@
 import { AveComponent, ComponentConfig, IComponentProps, registerComponent } from "../components";
 import { AppContainer } from "../renderer";
-import { HeaderItem, HeaderItemFormat, RichListBox as NativeRichListBox } from "ave-ui";
+import { HeaderItem, HeaderItemFormat, RichListBox as NativeRichListBox, RichListBoxItemVirtual } from "ave-ui";
 import { parseSize } from "./common";
 
 export interface IRichListBoxComponentProps extends IComponentProps {
 	headers: IRichListBoxHeader[];
+	data?: IRichListBoxData[];
 }
 
 export interface IRichListBoxHeader {
 	name: string;
 	size: string;
+	dataKey: string;
 	align?: HeaderItemFormat;
 }
+
+export type IRichListBoxData = Record<string, string>;
 
 class RichListBoxComponent extends AveComponent<IRichListBoxComponentProps> {
 	static tagName = "ave-rich-list-box";
 
 	private richListBox: NativeRichListBox;
+	private headers: IRichListBoxComponentProps["headers"];
 
 	protected onCreateUI() {
 		this.richListBox = new NativeRichListBox(this.window);
+		this.headers = [];
 		return this.richListBox;
 	}
 
@@ -27,6 +33,11 @@ class RichListBoxComponent extends AveComponent<IRichListBoxComponentProps> {
 		switch (propName) {
 			case "headers": {
 				this.setValueForHeaders(propValue ?? []);
+				break;
+			}
+
+			case "data": {
+				this.setValueForData(propValue ?? []);
 				break;
 			}
 		}
@@ -42,6 +53,18 @@ class RichListBoxComponent extends AveComponent<IRichListBoxComponentProps> {
 			const headerItem = new HeaderItem(headerDesc?.align ?? HeaderItemFormat.Center, headerDesc.name, parseSize(headerDesc.size));
 			header.Add(headerItem);
 		});
+		this.headers = [...headers];
+	}
+
+	private setValueForData(data: IRichListBoxComponentProps["data"]) {
+		this.richListBox.SetVirtual((sender, rowIndex, headerIndex) => {
+			const item = new RichListBoxItemVirtual();
+			const itemData = data[rowIndex];
+			const header = this.headers[headerIndex];
+			item.String = itemData[header.dataKey] ?? "";
+			return item;
+		});
+		this.richListBox.ItemSetCount(data.length);
 	}
 }
 
--- src/examples/rich-list-box/rich-list-box-basic.tsx
+++ src/examples/rich-list-box/rich-list-box-basic.tsx
@@ -4,13 +4,23 @@ import { DemoLayout } from "../common";
 
 export function TestRichListBoxBasic() {
 	const headers: IRichListBoxComponentProps["headers"] = [
-		{ name: "ID", size: "200dpx" },
-		{ name: "Name", size: "200dpx" },
+		{ name: "ID", size: "200dpx", dataKey: "id" },
+		{ name: "Name", size: "200dpx", dataKey: "name" },
 	];
+
+	const data: IRichListBoxComponentProps["data"] = [
+		{ id: "1", name: "a" },
+		{ id: "2", name: "b" },
+		{ id: "3", name: "c" },
+		{ id: "4", name: "d" },
+		{ id: "5", name: "e" },
+		{ id: "6", name: "f" },
+	];
+
 	return (
 		<Window title="RichListBox Basic">
 			<DemoLayout width="410dpx" height="300dpx">
-				<RichListBox headers={headers}></RichListBox>
+				<RichListBox headers={headers} data={data}></RichListBox>
 			</DemoLayout>
 		</Window>
 	);
