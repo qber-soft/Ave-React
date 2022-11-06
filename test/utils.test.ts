577e894f78177d59fdcf3b3978800ae453c847e6
rerender2021 <rerender2021@outlook.com>
2022-11-06 16:40:39 +0800

    add basic unit test

---
+++ test/utils.test.ts
@@ -0,0 +1,24 @@
+import { parseSize } from "../draft/ave-react/components/common/utils";
+
+describe("utils", () => {
+	test("parse size", () => {
+		{
+			const size = parseSize("100dpx");
+			expect(size.Value).toEqual(100);
+			expect(size.IsDpiScaled).toEqual(true);
+			expect(size.IsSlice).toEqual(false);
+		}
+		{
+			const size = parseSize("50px");
+			expect(size.Value).toEqual(50);
+			expect(size.IsDpiScaled).toEqual(false);
+			expect(size.IsSlice).toEqual(false);
+		}
+		{
+			const size = parseSize("1");
+			expect(size.Value).toEqual(1);
+			expect(size.IsDpiScaled).toEqual(false);
+			expect(size.IsSlice).toEqual(true);
+		}
+	});
+});
