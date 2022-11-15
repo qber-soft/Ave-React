a7a35f8475f08c9b3a616a539b5b02b3992e3113
rerender2021 <rerender2021@outlook.com>
2022-11-15 08:49:47 +0800

    impl font style

--- draft/ave-react/components/code-editor.tsx
+++ draft/ave-react/components/code-editor.tsx
@@ -1,13 +1,22 @@
-import { AveComponent, ComponentConfig, IComponentProps, registerComponent } from "./common";
+import { AveComponent, ComponentConfig, IComponentProps, IComponentStyle, registerComponent } from "./common";
 import { AppContainer } from "../renderer";
-import { CodeEditor as NativeCodeEditor, CodeEditorStyleItem, Vec4 } from "ave-ui";
+import { Byo2Font, CodeEditor as NativeCodeEditor, CodeEditorStyleItem, Vec4 } from "ave-ui";
 
 export interface ICodeEditorComponentProps extends IComponentProps {
+	style?: ICodeEditorStyle;
+}
+
+export interface ICodeEditorStyle extends IComponentStyle {
 	defaultVisualStyle?: ICodeEditorVisualStyle;
+	defaultFontStyle?: ICodeEditorFontStyle;
 }
 
 export interface ICodeEditorVisualStyle {
 	textColor?: Vec4;
+}
+
+export interface ICodeEditorFontStyle {
+	size?: number;
 }
 
 export enum StyleIndex {
@@ -28,15 +37,32 @@ class CodeEditorComponent extends AveComponent<ICodeEditorComponentProps> {
 
 	protected onUpdateProp(propName: keyof ICodeEditorComponentProps, propValue: any) {
 		switch (propName) {
-			case "defaultVisualStyle": {
-				const item = this.createStyleItem(propValue);
-				this.editor.VsSetStyle(StyleIndex.Default, item);
+			case "style": {
+				this.setValueForStyles(propValue ?? {});
 				break;
 			}
 		}
 	}
 
-	private createStyleItem(style: ICodeEditorVisualStyle = {}) {
+	private setValueForStyles(styles: ICodeEditorStyle = {}) {
+		(Object.keys(styles) as Array<keyof ICodeEditorStyle>).forEach((styleName) => {
+			switch (styleName) {
+				case "defaultVisualStyle": {
+					const item = this.createVisualStyleItem(styles.defaultVisualStyle ?? {});
+					this.editor.VsSetStyle(StyleIndex.Default, item);
+					break;
+				}
+
+				case "defaultFontStyle": {
+					const item = this.createFontStyleItem(styles.defaultFontStyle ?? {});
+					this.editor.VsSetFont(StyleIndex.Default, item);
+					break;
+				}
+			}
+		});
+	}
+
+	private createVisualStyleItem(style: ICodeEditorVisualStyle = {}) {
 		const item = new CodeEditorStyleItem();
 		for (const styleName in style) {
 			const styleValue = style[styleName];
@@ -47,6 +73,22 @@ class CodeEditorComponent extends AveComponent<ICodeEditorComponentProps> {
 				}
 			}
 		}
+		return item;
+	}
+
+	private createFontStyleItem(style: ICodeEditorFontStyle = {}) {
+		const fd = { ...this.window.GetTheme().GetFont() };
+		for (const styleName in style) {
+			const styleValue = style[styleName];
+			switch (styleName) {
+				case "size": {
+					fd.Size = styleValue;
+					break;
+				}
+			}
+		}
+
+		const item = new Byo2Font(this.window, fd);
 		return item;
 	}
 }
--- src/examples/code-editor/code-editor-basic.tsx
+++ src/examples/code-editor/code-editor-basic.tsx
@@ -1,17 +1,22 @@
 import { Vec4 } from "ave-ui";
 import React from "react";
-import { Window, CodeEditor, ICodeEditorComponentProps } from "../../ave-react";
+import { Window, CodeEditor, ICodeEditorStyle } from "../../ave-react";
 import { DemoLayout } from "../common";
 
-const defaultVisualStyle: ICodeEditorComponentProps["defaultVisualStyle"] = {
-	textColor: new Vec4(255, 0, 0, 255),
+const style: ICodeEditorStyle = {
+	defaultVisualStyle: {
+		textColor: new Vec4(0, 0, 0, 255),
+	},
+	defaultFontStyle: {
+		size: 13,
+	},
 };
 
 export function TestCodeEditorBasic() {
 	return (
 		<Window title="CodeEditor Basic">
 			<DemoLayout width="500dpx" height="300dpx">
-				<CodeEditor defaultVisualStyle={defaultVisualStyle}></CodeEditor>
+				<CodeEditor style={style}></CodeEditor>
 			</DemoLayout>
 		</Window>
 	);
