067f07e0b6fdf60bb4f592b7de7fadb283bf8c85
rerender2021 <rerender2021@outlook.com>
2023-01-14 11:34:20 +0800

    add test case: update text

--- test/e2e/hyperlink/index.test.tsx
+++ test/e2e/hyperlink/index.test.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Hyperlink } from "../../../src/ave-react";
-import { imageSnapshotTest, setupJest, TestContext } from "../common";
+import { getUpdateFunction, imageSnapshotTest, setupJest, TestContext } from "../common";
 import { toMatchImageSnapshot } from "jest-image-snapshot";
 import { getComponents } from "../../ave-testing";
 
@@ -10,6 +10,7 @@ setupJest();
 enum HyperlinkTestCases {
 	MountAndUnMount = "display hyperlink and remove",
 	// update props
+	UpdateText = "update text",
 }
 
 describe("label", () => {
@@ -37,5 +38,32 @@ describe("label", () => {
 			expect(components.length).toEqual(TestContext.defaultComponentCount + 1);
 			await imageSnapshotTest("root");
 		}
+	});
+
+	test(HyperlinkTestCases.UpdateText, async () => {
+		TestContext.updateTitle(HyperlinkTestCases.UpdateText);
+
+		let fireUpdate = null;
+		function TestCase() {
+			const [text, setText] = useState("<Ave Nodejs>");
+
+			useEffect(() => {
+				fireUpdate = getUpdateFunction(() => {
+					console.log(`update text`);
+					setText("<Ave React>");
+				});
+			}, []);
+			return (
+				<Grid id="root">
+					<Hyperlink text={text}></Hyperlink>
+				</Grid>
+			);
+		}
+
+		await TestContext.render(<TestCase />);
+		await imageSnapshotTest("root");
+
+		await fireUpdate();
+		await imageSnapshotTest("root");
 	});
 });
